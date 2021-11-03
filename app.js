b2184f00d51963af0a1df61ec63661ff1c014b72
Lussier07 <brendan.lussier07@gmail.com>
2021-11-03 16:46:09 -0500

    New constant

---
+++ app.js
@@ -0,0 +1,16 @@
+// Shuffle function from http://stackoverflow.com/a/2450976
+let shuffle = function(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+const cards = document.getElementById('cards');
